4b00adb7d11c131d6bd02556cfce87de24ba5493
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-03-07 01:30:31 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,6 +5,9 @@ def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
     if contestant["status"] == "Winner"
       return contestant["name"].split(" ")[0]
+          binding.pry
+    if contestant["status"] == "winner"
+      return contestant["name"]
     end
   end
 end
